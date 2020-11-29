f3c063793293491ca57ebb981a174d087dd80526
YourGuruNet <arvels123@inbox.lv>
2020-11-29 19:19:38 +0200

    Home section base

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    return <div>Hello from home</div>;
+    return (
+      <section className='header'>
+        <h1 className='header__text'></h1>
+        <button className='header__button'>Tours</button>
+      </section>
+    );
   }
 }
